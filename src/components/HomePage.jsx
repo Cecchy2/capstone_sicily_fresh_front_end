b82b5e4859207702956c282ad22315fb37020b08
Cecchy2 <dariocecchinato@gmail.com>
2024-10-01 17:11:29 +0200

    aggiunto effetto di scroll continuo dove incollerò le immagini delle ricette

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,30 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRicette } from "../redux/actions";
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const ricetteList = useSelector((state) => state.ricette.ricette);
+
+  useEffect(() => {
+    dispatch(getRicette());
+  }, [dispatch]);
+
   return (
     <>
-      <div className="homePage">
-        <Container>
-          <Row>
-            <Col></Col>
-          </Row>
-        </Container>
+      <div className="homePage"></div>
+      <div className="striscia">
+        <div className="carousel">
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+        </div>
       </div>
-      <div></div>
     </>
   );
 };
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,17 @@
 export const GET_RICETTE = "GET_RICETTE";
+
+export const getRicette = () => {
+  return async (dispatch) => {
+    const baseEndPoint = `http://localhost:3001/ricette`;
+    try {
+      const resp = await fetch(baseEndPoint);
+      if (resp.ok) {
+        const result = await resp.json();
+        console.log(result);
+        dispatch({ type: GET_RICETTE, payload: result });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};

70651367877efff26dd62b4da283d5602495e6b6
Cecchy2 <dariocecchinato@gmail.com>
2024-10-08 12:52:41 +0200

    aggiunto font lato a progetto

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -22,7 +22,7 @@ const HomePage = () => {
         <Container className="my-5">
           <Row className="justify-content-center text-center text-md-start">
             <Col className="ms-5">
-              <h1 className="display-5">ACQUISTA IL TUO ABBONAMENTO</h1>
+              <h1 className="f-lato-thin">ACQUISTA IL TUO ABBONAMENTO</h1>
               <div className="d-flex flex-column flex-md-row align-items-md-center ">
                 <h2 className="mb-3 mb-md-0 ms-5">SCEGLI LE TUE RICETTE</h2>
                 <Button variant="warning" className="border border-black ms-md-5">

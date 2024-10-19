6031aed38d9d6d00ede93976b6667babded93ba4
Cecchy2 <dariocecchinato@gmail.com>
2024-10-19 22:58:00 +0200

    aggiunto persistor ad app

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,9 @@
+// store.js
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+
 import ricetteReducer from "../reducers/ricetteReducer";
 import authReducer from "../reducers/authReducer";
 import utenteReducer from "../reducers/utenteReducer";
@@ -6,6 +11,14 @@ import abbonamentiReducer from "../reducers/abbonamentiReducer";
 import carrelliReducer from "../reducers/carrelliReducer";
 import carrelliDettagliReducer from "../reducers/carrelloDettaglioReducer";
 
+// Configurazione per Redux Persist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "utente"], // specifica i reducer da persistere
+};
+
+// Combina i tuoi reducer
 const rootReducer = combineReducers({
   ricette: ricetteReducer,
   auth: authReducer,
@@ -15,8 +28,23 @@ const rootReducer = combineReducers({
   carrelliDettagli: carrelliDettagliReducer,
 });
 
+// Crea un reducer persistito
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configura lo store con il reducer persistito e il middleware necessario
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignora questi tipi di azione per il controllo di serializzabilità
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+// Crea un persistor legato allo store
+export const persistor = persistStore(store);
+
+// Esporta lo store come default
 export default store;

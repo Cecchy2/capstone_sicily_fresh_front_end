091ade2dd8ba7582af664fca7e8c50cafb57bf00
Cecchy2 <dariocecchinato@gmail.com>
2024-10-21 17:46:43 +0200

    ricambiati endpoints

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -5,7 +5,7 @@ export const REGISTER = "REGISTER";
 export const login = (email, password) => {
   return async (dispatch) => {
     try {
-      const response = await fetch("https://sicilyfresh.netlify.app/authorization/login", {
+      const response = await fetch("https://occupational-rubia-cecchy-98f537b0.koyeb.app/authorization/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -44,7 +44,7 @@ export const logout = () => {
 
 export const register = (payload, avatar) => {
   return async (dispatch) => {
-    const baseEndPoint = `https://sicilyfresh.netlify.app/authorization/register`;
+    const baseEndPoint = `https://occupational-rubia-cecchy-98f537b0.koyeb.app/authorization/register`;
     const formData = new FormData();
 
     Object.keys(payload).forEach((key) => {
